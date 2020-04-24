60a7fddc563b0d33c994facca309d8d22695bbb0
mizan2832 <programmer620@gmail.com>
2020-04-25 00:21:16 +0600

    add works

---
+++ script.js
@@ -0,0 +1,9 @@
+function changebackground(x) {
+    x.style.backgroundImage = "none";
+    x.style.backgroundColor = "black";
+
+}
+
+function changeprevious(x) {
+    x.style.backgroundImage = "url('pro1.jpg')";
+}
